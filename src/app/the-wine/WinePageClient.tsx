ac9337b2813152e73adcb0b4ed0c843db6a9a047
Gleb <gleb@otherwine.co.uk>
2026-06-09 00:42:53 +0100

    Increase cream section bottle size by 10%

--- src/app/the-wine/WinePageClient.tsx
+++ src/app/the-wine/WinePageClient.tsx
@@ -336,7 +336,7 @@ export default function WinePageClient() {
             left: '50%',
             transform: 'translate(-50%, -50%) rotate(10deg)',
             width: 'auto',
-            height: '90svh',
+            height: '99svh',
             pointerEvents: 'none',
           }}
         />
