5018c968c57267e651c0d33679fa7534131aef48
Gleb <gleb@otherwine.co.uk>
2026-06-09 00:28:04 +0100

    Move oil bottle down 25px

--- src/app/the-wine/WinePageClient.tsx
+++ src/app/the-wine/WinePageClient.tsx
@@ -277,7 +277,7 @@ export default function WinePageClient() {
                   <img src={item.img} alt={item.name} style={{
                     position: 'absolute',
                     left: '50%',
-                    bottom: 'calc(clamp(12px, 2.03vw, 35px) - 105px)',
+                    bottom: 'calc(clamp(12px, 2.03vw, 35px) - 130px)',
                     transform: 'translateX(-50%) rotate(0.19deg) scale(2)',
                     width: '80vw',
                     height: 'auto',
