cb2aba57492b7b0beb396806fe07e4f930c5a3ea
Gleb <gleb@otherwine.co.uk>
2026-06-09 09:31:36 +0100

    Double rosé bottle size on mobile

--- src/app/the-wine/WinePageClient.tsx
+++ src/app/the-wine/WinePageClient.tsx
@@ -117,7 +117,7 @@ function WinePageMobile() {
                     <img
                       src="/52a8be1f462205b9d387a775e88e787e3ec36644.png"
                       alt={PRODUCTS[1].name}
-                      style={{ width: '75%', height: 'auto', display: 'block', pointerEvents: 'none', marginBottom: '0' }}
+                      style={{ width: '150%', height: 'auto', display: 'block', pointerEvents: 'none', marginBottom: '0' }}
                     />
                   )}
                   {i === 2 && (
