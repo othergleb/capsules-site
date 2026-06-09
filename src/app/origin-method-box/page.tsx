1ec80fd3c786e113a3ef685fbdf4d6fdcc6a464e
Gleb <gleb@otherwine.co.uk>
2026-06-09 10:13:48 +0100

    Adjust bottle: undo half horizontal move, nudge up slightly

--- src/app/origin-method-box/page.tsx
+++ src/app/origin-method-box/page.tsx
@@ -179,8 +179,8 @@ function OriginMethodBoxMobile() {
         {/* Cam 3 bottle — flipped vertically, rotated */}
         <div style={{
           position: 'absolute',
-          left: 'calc(-3.05vw - 90px + 50vw)',
-          top: 'calc(-41px + 30vw)',
+          left: 'calc(-3.05vw - 90px + 25vw)',
+          top: 'calc(-41px + 28vw)',
           width: '107.6vw',
           height: '141vw',
           display: 'flex',
