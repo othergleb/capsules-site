352bb041d75ad089c57f7bf907ad987eeb75dfe9
Gleb <gleb@otherwine.co.uk>
2026-06-09 11:00:50 +0100

    Nudge wine video down 5%

--- src/app/the-wine/WinePageClient.tsx
+++ src/app/the-wine/WinePageClient.tsx
@@ -364,7 +364,10 @@ export default function WinePageClient() {
           playsInline
           style={{
             position: 'absolute',
-            inset: 0,
+            top: '5%',
+            left: 0,
+            right: 0,
+            bottom: 0,
             width: '100%',
             height: '100%',
             objectFit: 'cover',
